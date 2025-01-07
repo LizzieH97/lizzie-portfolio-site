53329a5ddc4b8270eaa4a4699a418d06b11ff5c3
LizzieH97 <erhughes97@gmail.com#>
2025-01-07 16:04:44 +0000

    sorted out the market section layout

--- src/components/markets.jsx
+++ src/components/markets.jsx
@@ -2,15 +2,15 @@ import AltBackground from "./alt-background";
 
 export default function Markets() {
   const subtitle =
-    "col-start-2 text-2xl font-bold text-white p-2 pl-[35px] underline";
+    "col-start-2 text-2xl font-bold text-white p-0 pl-[35px] underline";
   const text =
-    "col-start-2 row-span-3 text-l font-bold text-white p-3 pt-[25px] text-center";
+    "col-start-2 row-span-3 text-l font-bold text-white p-0 text-center";
   return (
-    <div className="col-span-3 row-span-2 relative ml-[-200px] p-3">
-      <div className="col-span-2 row-start-3 grid grid-cols-[200px_150px_200px] grid-rows-[50px_auto_auto_auto] gap-4 pl-2">
+    <div className="col-start-1 col-end-3 row-start-3 relative p-0">
+      <div className="grid grid-cols-3 grid-rows-[50px_auto_auto_auto] gap-4 pl-2 justify-self-start">
         <h2 className={subtitle}>Markets</h2>
 
-        <AltBackground className="absolute top-0 left-0 w-full h-full z-0" />
+        <AltBackground className="relative top-0 left-0 w-full z-0" />
         <p className={text}>
           Here's a selection of photos from my markets - my very first market in
           Warrington, a market in New Brighton, and my Christmas market in
